39191d718d36d9b53bd31570b12eda527b5ad166
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2025-04-27 05:52:13 +0800

    Bug fixes
    Fixed initial default setting values too low
    Fixed - quick fix for audio changes while ingame
    Fixed level layout showing out of bounds

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Meowmeowzers-NinjaScrollAdventure-1.0";
+const cacheName = "Meowmeowzers-Ninja Scroll Adventure-1.1";
 const contentToCache = [
     "Build/7a2dbded24d57e056180125b1583e7c4.loader.js",
-    "Build/34c2fb9bcf40528b41ad5f26188ad6e0.framework.js",
-    "Build/95f02b2497df82c9b1b314b30d25938d.data",
-    "Build/87ccee1216e1c6c82fb9fed82ce3dda9.wasm",
+    "Build/7a0747bb277015ffcfc20d4d96296d04.framework.js",
+    "Build/330ff6e3f95ca582872736c6099f5dac.data",
+    "Build/013dad6e8ea802ca415d67c997bc1e9d.wasm",
     "TemplateData/style.css"
 
 ];
