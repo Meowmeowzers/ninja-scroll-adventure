1949a305b9f3937e82a47e6982f6d15b0c0c4966
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2025-04-27 02:41:00 +0800

    Added main files

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Meowmeowzers-NinjaScrollAdventure-0.9";
+const contentToCache = [
+    "Build/7a2dbded24d57e056180125b1583e7c4.loader.js",
+    "Build/34c2fb9bcf40528b41ad5f26188ad6e0.framework.js.br",
+    "Build/0216e77583f47d719b8f9b36a2d14ef6.data.br",
+    "Build/87ccee1216e1c6c82fb9fed82ce3dda9.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
